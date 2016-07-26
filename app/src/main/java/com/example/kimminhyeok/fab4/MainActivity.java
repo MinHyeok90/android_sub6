9a0dd5f316282e01a4556d2e966be4aaafa2231f
김민혁 <Kimminhyeok@kmh.local>
2016-07-26 17:25:54 +0900

    dialog

--- app/src/main/java/com/example/kimminhyeok/fab4/MainActivity.java
+++ app/src/main/java/com/example/kimminhyeok/fab4/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.kimminhyeok.fab4;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Context context = this;
+    TextView mTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +25,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
 
-            }
+        mTv = (TextView)findViewById(R.id.main_text_view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+        //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                        .setAction("Action", null).show();
+                        final EditText et = new EditText(MainActivity.this);
+                        AlertDialog.Builder ab = new AlertDialog.Builder(context);
+                        ab.setTitle("add string");
+//                        ab.setMessage("click add or exit");
+                        ab.setView(et)
+                          .setCancelable(false)
+                          .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialogInterface, int i) {
+                                  // if this button is clicked, close
+                                  // current activity
+//                                  MainActivity.this.finish(); //앱 종료하기.
+                                  mTv.setText(et.getText().toString());
+                                  et.setText("");
+                              }
+                          })
+                          .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialogInterface, int i) {
+                                  // if this button is clicked, just close
+                                  // the dialog box and do nothing
+                                  dialogInterface.dismiss();
+
+                              }
+                          });
+
+                        AlertDialog alertDialog = ab.create();
+                        alertDialog.show();
+
+                    }
         });
     }
 
